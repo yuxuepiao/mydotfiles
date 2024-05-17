ddf533daea9d47cd22e917df0e197328b4cba67f
Yuxue Piao <yuxue.piao@intel.com>
2024-05-16 23:29:25 -0700

    delete unused comment.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,24 +1,3 @@
 require("options")
 require("plugins")
 
---vim.api.nvim_create_autocmd("BufEnter",{
---  callback = function()
---    vim.lsp.start({
---      name = 'clangd',
---      cmd = {'/home/ypiao/clangd_17.0.3/bin/clangd'},
---      root_dir = vim.fn.getcwd(),
---    })
---  end,
---})
-
-
---print("Hello World");
-
--- concept of table, is data structure.
--- global table
-
--- setup lazy.nvim
-
--- :checkhealth lazy
--- all processes go as async
--- split plugin split files
